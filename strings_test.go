130c2f2a5126ea3c0fed3c842fb82b32cfb0aacc
rubiezzy <rubiezzy@student.21-school.ru>
2023-03-21 14:45:56 +0400

    Add table driven approach to the tests

--- strings_test.go
+++ strings_test.go
@@ -1,13 +1,27 @@
 package strings
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestReverseString(t *testing.T) {
-	string := "hello world"
-	reversedString := ReverseString(string)
-	if reversedString != "dlrow olleh" {
-		t.Fatalf("String is reversed incorrectly: %s", reversedString)
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{input: "hello world", expected: "dlrow olleh"},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.input, testReverseStringFunc(testCase.input, testCase.expected))
+	}
+}
+
+func testReverseStringFunc(input string, expected string) func(*testing.T) {
+	return func(t *testing.T) {
+		actual := ReverseString(input)
+		if actual != expected {
+			t.Errorf(fmt.Sprintf("Expected reversed string to be %s but instead got %s!", expected, actual))
+		}
 	}
 }
