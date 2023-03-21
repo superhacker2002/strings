2950ec5a11e873c718a1df7956ff2b8875713807
rubiezzy <rubiezzy@student.21-school.ru>
2023-03-21 21:40:27 +0400

    Make TestReverseString run in parallel

--- strings_test.go
+++ strings_test.go
@@ -5,6 +5,8 @@ import (
 )
 
 func TestReverseString(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
 		name           string
 		input          string
@@ -20,15 +22,11 @@ func TestReverseString(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		t.Run(testCase.name, testReverseStringFunc(testCase.input, testCase.expectedOutput))
-	}
-}
-
-func testReverseStringFunc(input string, expected string) func(*testing.T) {
-	return func(t *testing.T) {
-		actual := ReverseString(input)
-		if actual != expected {
-			t.Errorf("ReverseString(%q) = %q; expected %q", input, actual, expected)
-		}
+		t.Run(testCase.name, func(t *testing.T) {
+			actual := ReverseString(testCase.input)
+			if actual != testCase.expectedOutput {
+				t.Errorf("ReverseString(%q) = %q; expected %q", testCase.input, actual, testCase.expectedOutput)
+			}
+		})
 	}
 }
